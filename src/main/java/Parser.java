c4e9fd99c56747f40b797cf80446fc66ee5d91e0
MaximKhamzin <openpw@icloud.com>
2022-05-21 10:30:01 +0500

    1.2

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,24 +1,46 @@
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Parser {
-
-    public static void main(String[] args) throws IOException {
-        Document page = getPage();
-        // css query language
-        Element tableWidth = page.select("Table[class=wt]").first();
-        System.out.println(tableWidth);
-        String date = "";
-        System.out.println(date + "    Явление    Температура    Давление    Влажность    Ветер");
-    }
 
     private static Document getPage() throws IOException{
         String url = "https://pogoda.spb.ru/";
         Document page = Jsoup.parse(new URL(url), 3000);
         return page;
+    }
+
+    // 22.04 Суббота погода сегодня
+    // 22.04
+    // \d{2}\.\d{2}
+    private static Pattern pattern = Pattern.compile("\\d{2}\\.\\d{2}");
+
+    private static String getDateFromString(String stringDate) throws Exception {
+        Matcher matcher = pattern.matcher(stringDate);
+        if (matcher.find()) {
+            return matcher.group();
+        }
+        throw new Exception("Can't extract date from string");
+    }
+
+    public static void main(String[] args) throws Exception {
+        Document page = getPage();
+        // css query language
+        Element tableWth = page.select("Table[class=wt]").first();
+        Elements names = tableWth.select("tr[class=wth]");
+        Elements values = tableWth.select("tr[valign=top]");
+
+        for (Element name : names) {
+            String dateString = name.select("th[id=dt]").text();
+            String date = getDateFromString(dateString);
+            System.out.println(date + "    Явление    Температура    Давление    Влажность    Ветер");
+        }
+        
     }
 }
